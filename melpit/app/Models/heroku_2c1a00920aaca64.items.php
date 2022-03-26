811cb69b5e498c3d0e516b45ffd9f0f604e68b8a
tyukei <peace10000@icloud.com>
2022-03-26 20:56:45 +0900

    add db

---
+++ melpit/app/Models/heroku_2c1a00920aaca64.items.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class heroku_2c1a00920aaca64.items extends Model
+{
+    use HasFactory;
+
+    protected $table = "items";
+}
