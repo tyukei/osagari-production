426567cee8ce676cd50a6f036aca5c24e21b948b
tyukei <peace10000@icloud.com>
2022-03-26 21:12:07 +0900

    db

--- melpit/app/Models/Items.php
+++ melpit/app/Models/Items.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class heroku_2c1a00920aaca64.items extends Model
+class Items extends Model
 {
     use HasFactory;
 
