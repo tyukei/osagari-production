bbf8e5d7a6728066fa321a5011bddc6850d270ec
tyukei <peace10000@icloud.com>
2022-03-28 18:58:03 +0900

    chnage

--- melpit/app/Providers/AppServiceProvider.php
+++ melpit/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Payjp\Payjp;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,11 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Payjp::setApiKey(config('payjp.secret_key'));
-
-Paginator::useBootstrap();
-Schema::defaultStringLength(191);
-if (request()->isSecure()) {
-    \URL::forceScheme('https');
-}
+        Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
